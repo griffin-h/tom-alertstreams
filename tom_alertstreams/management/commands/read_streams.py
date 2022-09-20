8c90a0f5736b3b6d493d2a6e607f96bedde519ab
Lindy Lindstrom <llindstrom@lco.global>
2022-09-20 00:52:47 +0000

    first cut at using multiprocessing to read multiple streams

--- tom_alertstreams/management/commands/read_streams.py
+++ tom_alertstreams/management/commands/read_streams.py
@@ -1,4 +1,5 @@
 import logging
+import multiprocessing
 
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
@@ -7,22 +8,22 @@ from tom_alertstreams.alertstreams.alertstream import get_default_alert_streams
 
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+#logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 
 class Command(BaseCommand):
     help = 'Consume alerts from the alert streams configured in the settings.py ALERT_STREAMS'
 
 
     def handle(self, *args, **options):
-        logger.debug(f'args: {args}')
-        logger.debug(f'options: {options}')
+        logger.debug(f'read_streams.Command.handle() args: {args}')
+        logger.debug(f'read_streams.Command.handle() options: {options}')
 
     
-        logger.info('read_streams finding alert_streams...')
         for alert_stream in get_default_alert_streams():
-            logger.info(f'read_streams listening to alert_stream: {alert_stream._get_stream_classname()}')
-            alert_stream.listen()  # does not return
-            logger.warning(f'*** Returned from {alert_stream._get_stream_classname()}')
+            p = multiprocessing.Process(target=alert_stream.listen)
+            p.start()  # PID not assigned until p.start()
+            logger.info(f'read_streams {alert_stream._get_stream_classname()} PID={p.pid}')
 
         return
 
