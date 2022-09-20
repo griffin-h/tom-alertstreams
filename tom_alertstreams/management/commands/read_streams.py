65069b184e7998bfd7a9594dc6682b11e2447365
Lindy Lindstrom <llindstrom@lco.global>
2022-09-20 21:26:10 +0000

    use threading instead of multiprocessing to read multiple streams
    Thread are sufficient and don't leave orphaned processes about.

--- tom_alertstreams/management/commands/read_streams.py
+++ tom_alertstreams/management/commands/read_streams.py
@@ -1,20 +1,18 @@
 import logging
-from multiprocessing import Process
+from threading import Thread
 
 from django.core.exceptions import ImproperlyConfigured
-from django.core.management.base import BaseCommand, CommandError
-from django.conf import settings
+from django.core.management.base import BaseCommand
 
 from tom_alertstreams.alertstreams.alertstream import get_default_alert_streams
 
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+#logger.setLevel(logging.DEBUG)
 logger.setLevel(logging.INFO)
 
 class Command(BaseCommand):
     help = 'Consume alerts from the alert streams configured in the settings.py ALERT_STREAMS'
-
 
     def handle(self, *args, **options):
         logger.debug(f'read_streams.Command.handle() args: {args}')
@@ -26,22 +24,13 @@ class Command(BaseCommand):
             logger.error(err)
             exit(1)
 
-        alert_stream_processes = []
         try:
+            # listen to each alert_stream in it's own Thread (sort of at the same time)
             for alert_stream in alert_streams:
-                p = Process(target=alert_stream.listen)
-                alert_stream_processes.append(p)  # save Process instance for clean-up 
-                p.start()  # PID not assigned until p.start()
-                logger.info(f'read_streams {alert_stream._get_stream_classname()} PID={p.pid}')
+                t = Thread(target=alert_stream.listen, name=alert_stream._get_stream_classname())
+                t.start()
+                logger.info(f'read_streams {alert_stream._get_stream_classname()} TID={t.native_id} ; thread identifier={t.ident}')
         except KeyboardInterrupt as msg:
-            logger.info(f'read_streams handling KeyboardInterupt: {msg}')
-            for p in alert_stream_processes:
-                if p.is_alive():
-                    logger.info(f'read_streams Terminating {alert_stream._get_stream_classname()} PID={p.pid}')
-                    p.terminate()
-                else:
-                    logger.info(f'read_streams {alert_stream._get_stream_classname()} PID={p.pid} exit code: {p.exitcode}')
+            logger.info(f'read_streams handling KeyboardInterupt {msg}')
 
-        logger.info(f'read_streams.Command.handle() exiting.')
-        return
-
+        logger.info('read_streams Command.handle() returning...')
