0d47bfb36f30c0543872407326563d93e523d525
Lindy Lindstrom <llindstrom@lco.global>
2022-09-20 18:15:51 +0000

    try to handle process clean-up better (not always working)

--- tom_alertstreams/management/commands/read_streams.py
+++ tom_alertstreams/management/commands/read_streams.py
@@ -1,5 +1,5 @@
 import logging
-import multiprocessing
+from multiprocessing import Process
 
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
@@ -8,7 +8,7 @@ from tom_alertstreams.alertstreams.alertstream import get_default_alert_streams
 
 
 logger = logging.getLogger(__name__)
-#logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.DEBUG)
 logger.setLevel(logging.INFO)
 
 class Command(BaseCommand):
@@ -19,11 +19,22 @@ class Command(BaseCommand):
         logger.debug(f'read_streams.Command.handle() args: {args}')
         logger.debug(f'read_streams.Command.handle() options: {options}')
 
-    
-        for alert_stream in get_default_alert_streams():
-            p = multiprocessing.Process(target=alert_stream.listen)
-            p.start()  # PID not assigned until p.start()
-            logger.info(f'read_streams {alert_stream._get_stream_classname()} PID={p.pid}')
+        alert_stream_processes = []
+        try:
+            for alert_stream in get_default_alert_streams():
+                p = Process(target=alert_stream.listen)
+                alert_stream_processes.append(p)  # save Process instance for clean-up 
+                p.start()  # PID not assigned until p.start()
+                logger.info(f'read_streams {alert_stream._get_stream_classname()} PID={p.pid}')
+        except KeyboardInterrupt as msg:
+            logger.info(f'read_streams handling KeyboardInterupt: {msg}')
+            for p in alert_stream_processes:
+                if p.is_alive():
+                    logger.info(f'read_streams Terminating {alert_stream._get_stream_classname()} PID={p.pid}')
+                    p.terminate()
+                else:
+                    logger.info(f'read_streams {alert_stream._get_stream_classname()} PID={p.pid} exit code: {p.exitcode}')
 
+        logger.info(f'read_streams.Command.handle() exiting.')
         return
 
